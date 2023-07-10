f1b979468876b30636b2ce9390fada5e612274a2
Ishaan Khullar <ishaan.khullar06@gmail.com>
2023-07-10 12:06:03 +0530

    Added gain variables!

--- QUADCOPTERV1_BETA_1.0/flight_computer/flight_computer.ino
+++ QUADCOPTERV1_BETA_1.0/flight_computer/flight_computer.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
 
+
+float currTime = 0;
+float prevTime = 0;
+
+float Kp[3] = {1, 1, 1};
+float Ki[3] = {1, 1, 1};
+float Kd[3] = {1, 1, 1};
+
+
+void setup() {
+
+  Serial.begin(9600);   
+
+  Wire.begin();                           //begin the wire comunication  
+  Wire.beginTransmission(0x68);           //begin, Send the slave adress (in this case 68)              
+  Wire.write(0x6B);                       //make the reset (place a 0 into the 6B register)
+  Wire.write(0x00);
+  Wire.endTransmission(true);             //end the transmission
+  
+  Wire.beginTransmission(0x68);           //begin, Send the slave adress (in this case 68) 
+  Wire.write(0x1B);                       //We want to write to the GYRO_CONFIG register (1B hex)
+  Wire.write(0x10);                       //Set the register bits as 00010000 (1000dps full scale)
+  Wire.endTransmission(true);             //End the transmission with the gyro
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  currTime = millis();
 
+
+
+
+  prevTime = currTime;
 }
